9855528982c8f1ab667e94f4fa62f2cf45a61bfd
Jacob Chen <chenjr0719@gmail.com>
2022-02-16 18:48:39 +0800

    feat: Add logger

--- gogolook/config.py
+++ gogolook/config.py
@@ -1,6 +1,10 @@
-from ast import Str
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 
 class Settings(BaseSettings):
     DATABASE_URI: str
+    LOG_LEVEL: str = "info"
+
+    @validator("LOG_LEVEL", allow_reuse=True)
+    def get_log_level(cls, v: str) -> str:
+        return v.lower()
